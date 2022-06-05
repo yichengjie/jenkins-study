47c3bafe3770b3a12e1296834f6549421887ad01
易成杰 <626659321@qq.com>
2022-06-05 20:56:13 +0800

    tomcat目录修改

--- jenkins-execute.sh
+++ jenkins-execute.sh
@@ -2,6 +2,6 @@ BUILD_ID=DONTKILLME
 . /etc/profile
 # 配置运行参数
 export PROJ_PATH=`pwd`
-export TOMCAT_APP_PATH=/usr/local/tomcat/webapp
+export TOMCAT_APP_PATH=/usr/local/tomcat
 # 执行运行脚本
 sh $PROJ_PATH/jenkins-study/deploy.sh
