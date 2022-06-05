5d7c6f798dec61b5d5012f0cc6e4318527706f91
易成杰 <626659321@qq.com>
2022-06-05 20:04:22 +0800

    补充部署shell

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# 需要配置如下参数
+# 项目路径，在Execute Shell中配置项目路径，pwd就可以获得该项目路径
+# export PROJ_PATH=这个jenkins任务在部署机器上的路径
+# export TOMCAT_APP_PATH=tomcat在部署机器上的路径
+killTomcat(){
+  pid=`ps -ef|grep jenkins-study| grep java|awk '{print $2}'`
+  echo "tomcat Id list : $pid"
+  if [ "$pid" = "" ]
+  then
+    echo "no tomcat pid alive"
+  else
+    kill -9 $pid
+  fi
+}
+
+cd $PROJ_PATH/jenkins-study
+mvn clean install
+
+# 停止tomcat
+killTomcat
+
+# 删除原有工程
+rm -f $TOMCAT_APP_PATH/webapps/jenkins-study-*.jar
+
+# 复制新的工程
+cp $PROJ_PATH/jenkins-study/target/jenkins-study-1.0-SNAPSHOT.jar $TOMCAT_APP_PATH/webapps/
+
+cd $TOMCAT_APP_PATH/webapps/
+
+sh java -jar jenkins-study-1.0-SNAPSHOT.jar
---
+++ jenkins-execute.sh
@@ -0,0 +1,7 @@
+BUILD_ID=DONTKILLME
+. /etc/profile
+# 配置运行参数
+export PROJ_PATH=`pwd`
+export TOMCAT_APP_PATH=/usr/local/tomcat/webapp
+# 执行运行脚本
+sh $PROJ_PATH/jenkins-study/deploy.sh
